b00152d71c3c2d17f3b1a62c32dca69a16278745
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-04-18 16:05:47 +0400

    js enter

---
+++ JavaScript/PureJS/lesson2-datatypes/js/script.js
@@ -0,0 +1,43 @@
+// Primitive data types
+// string - metn
+// var info = "Hello ";
+// console.log(info);
+
+
+// number - reqem
+// var info = 5;
+// var infoOne = "10";
+// console.log(info + infoOne);
+// boolean - mentiqi
+// true false
+// if (false) {
+//     console.log('bu duzdur');
+// }else{
+//     console.log('bu sehvdir');
+// }
+
+
+
+// Composite data types
+// array 
+// var info = [1,"Hello",true];
+// console.log(info);
+// object
+// var car = {
+//     brand:"BMW",
+//     model:"X5",
+//     newCar:true,
+//     year:2023,
+//     color:["white","black",'red'],
+
+// }
+// console.log(car);
+
+
+
+
+// Special data types
+// undefined
+var a = null;
+console.log(a);
+// null
