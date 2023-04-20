4364e5a873c159fa6bb6ab7d7e4ebaf779492ec5
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-04-20 16:23:47 +0400

    operators

---
+++ JavaScript/PureJS/lesson4-variables/js/script.js
@@ -0,0 +1,61 @@
+
+ 
+
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var        -> variable (global)
+// let        -> let      (local)
+// const      -> constant (local)
+
+
+
+// // global
+// if (true) {
+//     // local global
+//     if (true) {
+//         // local global
+//         if (true) {
+//             let a =  5;
+//             // local
+            
+//         }
+//         console.log(a);
+//     }
+// }
+// let a = 30;
+
+
+// if (true) {
+//     let a = 10;
+// }
+
+// if (true) {
+//     let a = 20;
+// }
+
+// console.log(a);
