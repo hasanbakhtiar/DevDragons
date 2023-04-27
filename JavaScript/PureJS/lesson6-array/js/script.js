b8df0a69b0e63ae82e40f5cc917859b9815c6f45
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-04-27 16:31:48 +0400

    array

--- JavaScript/PureJS/lesson6-array/js/script.js
+++ JavaScript/PureJS/lesson6-array/js/script.js
@@ -78,27 +78,29 @@
 
 
 
-const infoPartOne = ['a','b','c',[1,2,3,['1a','2a','3a','4a','5a'],4,5],'d','e'];
-const infoPartTwo = ['f','g','h',[6,7,8,['1b','2b','3b','4b','5b'],9,10],'i','n'];
+// const infoPartOne = ['a','b','c',[1,2,3,['1a','2a','3a','4a','5a'],4,5],'d','e'];
+// const infoPartTwo = ['f','g','h',[6,7,8,['1b','2b','3b','4b','5b'],9,10],'i','n'];
 
 
-// part one start
-const letterAndNumberPartOne = infoPartOne[3][3];
-const letterAndNumberPartTwo = infoPartTwo[3][3];
-const totalLetterAndNumber = letterAndNumberPartOne.concat(letterAndNumberPartTwo);
-console.log(totalLetterAndNumber);
-// part one end
+// // part one start
+// const letterAndNumberPartOne = infoPartOne[3][3];
+// const letterAndNumberPartTwo = infoPartTwo[3][3];
+// const totalLetterAndNumber = letterAndNumberPartOne.concat(letterAndNumberPartTwo);
+// console.log(totalLetterAndNumber);
+// // part one end
 
-// part two start
-const onlyNumberPartOne = infoPartOne[3].slice(0,3).concat(infoPartOne[3].slice(4,6));
-const onlyNumberPartTwo = infoPartTwo[3].slice(0,3).concat(infoPartTwo[3].slice(4,6));
-const totalOnlyNumber = onlyNumberPartOne.concat(onlyNumberPartTwo);
-console.log(totalOnlyNumber);
+// // part two start
+// const onlyNumberPartOne = infoPartOne[3].slice(0,3).concat(infoPartOne[3].slice(4,6));
+// const onlyNumberPartTwo = infoPartTwo[3].slice(0,3).concat(infoPartTwo[3].slice(4,6));
+// const totalOnlyNumber = onlyNumberPartOne.concat(onlyNumberPartTwo);
+// console.log(totalOnlyNumber);
 
 // part two end
 
 
 
+const a = window.prompt();
+console.log(a);
 
 
 
@@ -110,6 +112,25 @@ console.log(totalOnlyNumber);
 
 
 
+
+
+
+
+const productOne = ['onion','milk','egg',['meat','cola','fruits','fish',['bread','water','oil']]]; //200
+const productTwo = ['middle onion',' middle milk','20 egg',['middle meat',' middle cola',' middle fruits','fish',[' middle bread','middle water','middle oil']]]; //200
+const productThree = ['big onion','big milk','big egg',['big meat','big cola','big fruits','big fish',['big bread','big water','big oil']]]; //200
+
+
+// 50-100 -> productOne1 + productThree1
+// 100-150 -> productOne2 + productTwo3
+// 150-200 -> productOne1,2 + productThree3
+// 200-250 -> productOne1,2,3 + productThree2,3
+// 250-300 -> productOne2,3 + productTwo1,2
+// 300-350 -> productTwo12 + productThree3
+// 350-400 -> productOne3 + productThree1,2,3
+// 400-450 -> productOne1,2,3 + productThree,1,2,3
+// 450-500 -> productOne3 + productThree1,2 + productTwo1,2
+// 500-550 -> productOne+productTwo+productThree
 
 
 
