1a2c6367824db75c1518f5915032b8edf3a514e1
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-04-27 15:37:02 +0400

    array

---
+++ JavaScript/PureJS/lesson6-array/js/script.js
@@ -0,0 +1,95 @@
+// const product = ['table','chair','sofa'];
+// console.log(product[2]);
+
+// const product = new Array('table','chair','sofa');
+// console.log(product);
+
+// const product = [];
+// product[0] = 'table';
+// product[1] = 'bed';
+// product[2] = 5;
+// console.log(product);
+
+// const product = ['table','chair','sofa'];
+// product[1] = 'updated';
+// console.log(product);
+
+
+// const product = ['table','chair','sofa'];
+// product[3] = 'new product';
+// product['mykey'] = 'new value';
+
+// console.log(product['mykey']);
+
+
+
+// const product = ['table','chair',[1,2,[true,['hello','data','info',['call me']]],3,4],'sofa'];
+// console.log(product[2][2][1][3][0]);
+
+// const product = ['table','chair','sofa'];
+
+// product.push('new value');
+// product.pop();
+// product.shift();
+// product.unshift('new value');
+// console.log(product);
+
+// const productOne = ['table','chair','sofa'];
+// const productTwo =  ['milk','egg'];
+// const totalProduct = productOne.concat(productTwo);
+// console.log(totalProduct);
+// console.log(productOne,productTwo);
+
+// console.log(product[0].length);
+
+
+// const productOne = ['table','chair','sofa'];
+// console.log(productOne.slice(0,2));
+// console.log(console);
+// productOne.splice(1,1,"test",'hello');
+// console.log(productOne);
+
+// console.log(productOne.join(" | "));
+// console.log(productOne.sort());
+// console.log(productOne.reverse());
+// const info = productOne.sort();
+// console.log(info.reverse());
+
+// debugger;        
+// "use strict";
+
+
+// let a = 5;
+// console.log(a);
+
+
+
+// const data = "Hello";
+// console.log(data.substring(0,1));
+
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+
+
+
+
+
+
+['a','b','c',[1,2,3,['1a','2a','3a','4a','5a'],4,5],'d','e']
+
+['f','g','h',[6,7,8,['1b','2b','3b','4b','5b'],9,10],'i','n']
+
+
+
+
+
+
+
+
+
+
+
+
