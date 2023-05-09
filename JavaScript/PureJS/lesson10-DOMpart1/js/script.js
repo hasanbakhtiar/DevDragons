0b36e447a40648fe90a888efecb293ffc8ba5218
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-05-09 16:30:09 +0400

    DOM enter

---
+++ JavaScript/PureJS/lesson10-DOMpart1/js/script.js
@@ -0,0 +1,18 @@
+
+// function showCapital(){
+//     document.getElementsByTagName('h1')[0].innerHTML = 'London '
+// }
+
+
+
+// document.getElementsByTagName('button')[0].addEventListener('click',showCapital);
+// document.getElementsByTagName('button')[0].onclick=showCapital;
+
+
+
+// import { products } from "../data/products.js";
+
+// products.map(item=>{
+//     console.log(item.title)
+// })
+
