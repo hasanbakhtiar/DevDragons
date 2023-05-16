c3d5b82347bf9f96eac931b4fc90d1a24b8d7ef9
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-05-16 15:20:23 +0400

    DOM part 2

---
+++ JavaScript/PureJS/lesson11-DOMpart2/js/script.js
@@ -0,0 +1,18 @@
+// const myLi = document.getElementsByTagName('li');
+// for(let c = 0; c<myLi.length; c++){
+//     myLi[c].innerHTML = "Bye";
+// }
+// document.getElementsByClassName('test')[0].innerHTML = 'bye';
+// document.getElementById('demo').innerHTML = 'memo';
+
+
+// ===================================================================
+
+
+// document.querySelector('ul .test > span').innerHTML = 'test';
+
+// for(let i=0; i<14;i++){
+//     document.querySelectorAll('li')[i].innerHTML = 'test';
+// }
+
+
