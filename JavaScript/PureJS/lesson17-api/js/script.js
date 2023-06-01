6dac2d80f2dfd362cc73f1509c63345fceb7cf26
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-06-01 16:09:18 +0400

    api

--- JavaScript/PureJS/lesson17-api/js/script.js
+++ JavaScript/PureJS/lesson17-api/js/script.js
@@ -13,13 +13,38 @@
 // .catch(b=>console.log(b))
 
 const ol = document.querySelector('ol');
-fetch('https://fakestoreapi.com/products')
+const btn = document.querySelectorAll('button');
+let btncat = '';
+
+
+
+const fetchApi = ()=>{
+    fetch('https://fakestoreapi.com/products')
 .then(res=>res.json())
 .then(data=>{
     let li = "";
     data.map(item=>{
-        li+=`<li class="text-warning"><img width="100" src="${item.image}"/>${item.title}</li>`
+        if (item.category ===  btncat) {
+            li+=`<li class="text-warning"><img width="100"
+        src="${item.image}"/>${item.title}</li>`
+        }
     })
     ol.innerHTML = li;
 })
 .catch(err=>console.log(err))
+}
+
+
+
+btn[0].onclick=()=>{
+    btncat = 'men\'s clothing';
+    fetchApi();
+}
+btn[1].onclick=()=>{
+    btncat = 'women\'s clothing';
+    fetchApi(); 
+}
+
+fetchApi(); 
+
+
