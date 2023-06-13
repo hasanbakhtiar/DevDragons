dd44f3dd1886062d0e03a7f90369843a4a7ed0de
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-06-13 15:17:45 +0400

    ajax

---
+++ JavaScript/PureJS/lesson20-AJAX-sync-await/js/script.js
@@ -0,0 +1,19 @@
+// const fetchApi = async()=>{
+//    const data =  await fetch('https://jsonplaceholder.typicode.com/users');
+//    const comingdata = await data.json();
+//    console.log(comingdata);
+// }
+// fetchApi();
+
+
+// const btn  = document.querySelector('button');
+// btn.onclick = ()=> {
+
+//     const xhttp = new XMLHttpRequest();
+
+//     xhttp.onload = function() {
+//       document.querySelector("ol").innerHTML = this.responseText;
+//       }
+//     xhttp.open("GET", "../content/hello.txt", true);
+//     xhttp.send();
+//   }
