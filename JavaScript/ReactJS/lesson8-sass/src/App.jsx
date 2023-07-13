23dd16b1b8e8f9f004862e11bfb1150993a0ef0e
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-13 15:56:18 +0400

    sass

---
+++ JavaScript/ReactJS/lesson8-sass/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='container'>
+
+<div className="main-box">
+  <div className="box">1</div>
+  <div className="box">2</div>
+  <div className="box">3</div>
+  <div className="box">4</div>
+  <div className="box">5</div>
+</div>
+
+<ol>
+  <li>testing</li>
+  <li>testing</li>
+  <li>testing</li>
+  <li>testing</li>
+  <li>testing</li>
+  <li>testing</li>
+  <li>testing</li>
+  <li>testing</li>
+  <li>testing</li>
+  <li>testing</li>
+
+</ol>
+<h1>testing</h1>
+    </div>
+  )
+}
+
+export default App
