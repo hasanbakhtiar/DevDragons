d920ded4a9351e0fa4852800659fcd68308980e0
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-07-25 13:59:41 +0400

    filter search

---
+++ JavaScript/ReactJS/lesson12-filter-search/src/components/Header.jsx
@@ -0,0 +1,64 @@
+import { useContext, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
+import { ProductContext } from '../context/ProductProvider';
+
+const Header = () => {
+  const [query, setQuery] = useState(null);
+  const [product] = useContext(ProductContext);
+
+  return (
+    <Navbar expand="lg" className="bg-dark navbar-dark">
+      <Container>
+        <Navbar.Brand href="#home">Techno Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <LinkContainer to="/"><Nav.Link >Home</Nav.Link></LinkContainer>
+            <LinkContainer to="/products"><Nav.Link >Products</Nav.Link></LinkContainer>
+          </Nav>
+
+          <div >
+            <button type="button" className="btn btn-primary me-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+            <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">Search</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+                  </div>
+                  <div className="modal-body">
+                    <form className="input-group mb-3">
+                      <input onChange={e => setQuery(e.target.value)} type="text" className="form-control" placeholder="Enter product name" aria-label="Enter product name" aria-describedby="button-addon2" />
+                      <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                    </form>
+                    
+                    <ul className="list-group">
+                      {product.filter(p => p.title.toLowerCase().includes(query)).map(item => (
+                        <li key={item.id} className="list-group-item d-flex justify-content-between">{item.title}<img width={50} src={item.images[0]} /></li>
+                      ))}
+                    </ul>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          {localStorage.getItem('active') === 'true' ? "hasan@webluna.az" : <LinkContainer to="/login"><Button variant='success'>Login</Button></LinkContainer>}
+
+        </Navbar.Collapse>
+
+      </Container>
+    </Navbar>
+
+  )
+}
+
+export default Header
---
+++ JavaScript/ReactJS/lesson12-filter-search/src/pages/Products.jsx
@@ -0,0 +1,55 @@
+
+import { useContext, useState } from 'react';
+import SingleCard from '../components/SingleCard';
+import { Col, ListGroup, Row } from 'react-bootstrap'
+import { ProductContext } from '../context/ProductProvider';
+
+const Products = () => {
+  const [product] = useContext(ProductContext);
+  const [catProduct,setCatProduct] = useState([]);
+  const handleClick = (cat)=>{
+   const catData = product.filter(p=>p.category === cat);
+   setCatProduct(catData)
+  }
+  return (
+    <>
+      <h1 className='text-center my-5 text-light'>Product List</h1>
+      <Row>
+      <Col sm={12} md={3}>
+      <ListGroup>
+      <ListGroup.Item onClick={()=>{handleClick("smartphones")}}>Smartphones</ListGroup.Item>
+      <ListGroup.Item onClick={()=>{handleClick("laptops")}}>Laptops</ListGroup.Item>
+      <ListGroup.Item onClick={()=>{handleClick("fragrances")}}>Fragrances</ListGroup.Item>
+      <ListGroup.Item onClick={()=>{handleClick("skincare")}}>Skincare</ListGroup.Item>
+      <ListGroup.Item onClick={()=>{handleClick("groceries")}}>Groceries</ListGroup.Item>
+    </ListGroup>
+      </Col>
+      <Col sm={12} md={9}>
+
+       <Row className='g-5'>
+       {catProduct.length === 0 ? product.map(item=>(
+          <SingleCard 
+          key={item.id}
+          photo={item.images[0]}
+          title={item.title}
+          price={item.price}
+          id={item.id}
+          />
+        )) : catProduct.map(item=>(
+          <SingleCard 
+          key={item.id}
+          photo={item.images[0]}
+          title={item.title}
+          price={item.price}
+          id={item.id}
+          />
+        ))}
+       </Row>
+        
+      </Col>
+    </Row>
+    </>
+  )
+}
+
+export default Products
