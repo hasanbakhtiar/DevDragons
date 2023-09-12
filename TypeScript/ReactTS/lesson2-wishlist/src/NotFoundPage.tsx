cabd68a332a6d284844341e084f1339c8cbaf13f
HasanBakhtiar <hasan.doctype@gmail.com>
2023-09-12 12:27:56 +0400

    change name

--- TypeScript/ReactTS/lesson2-wishlist/src/NotFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom'
-
-const NotFoundPage = () => {
-  return (
-    <Link to="/"><img src="https://freefrontend.com/assets/img/html-funny-404-pages/CodePen-404-Page.gif" alt="" /></Link>
-  )
-}
-
-export default NotFoundPage
